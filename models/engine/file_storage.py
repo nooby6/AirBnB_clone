298e0d67a459381a442fac86f3578be5ea3d7ba0
kimaita <johnnieqaym@gmail.com>
2024-02-09 02:17:33 +0300

    added functionality

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 
 import json
 
+
 class FileStorage():
     """Serializes all instances of BaseClass to a JSON file and
     deserializes a JSON file to BaseModel instances.
@@ -28,10 +29,9 @@ class FileStorage():
         Args:
             obj : a BaseModel object dictionary
         """
-        from models.base_model import BaseModel
 
         if isinstance(obj, dict):
-            obj = BaseModel(**obj)
+            obj = self.create_new(obj.get('__class__'), obj)
 
         obj_key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects.update({obj_key: obj})
@@ -58,7 +58,82 @@ class FileStorage():
                 if not json_str:
                     return
                 json_objs = json.loads(json_str)
+
                 for obj_dict in json_objs.values():
                     self.new(obj_dict)
         except FileNotFoundError:
             pass
+
+    def create_new(self, classname, args=None):
+        """Create a new instance of the specified class.
+
+        Args:
+            classname (str): The name of the class to instantiate.
+        """
+        
+        # Dynamically import the class
+        class_map = {'BaseModel': 'base_model',
+                     'User': 'user',
+                     'Place': 'place',
+                     'Amenity': 'amenity',
+                     'State': 'state',
+                     'City': 'city',
+                     'Review': 'review'}
+
+        file_name = class_map.get(classname)
+        module = __import__(f"models.{file_name}", fromlist=[classname])
+
+        cls = getattr(module, classname)               
+        return cls(args)
+    
+    def get_instance(self, key):
+        """_summary_
+
+        Args:
+            id (_type_): _description_
+        """
+        
+        return self.__objects.get(key)
+
+    def destroy_instance(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+        """
+
+        if key in self.__objects:
+            self.__objects.pop(key)
+            self.save()
+            return True
+        
+    def get_class_instances(self, classname=None):
+        """Retrieve all instances of a given class or all saved instances
+        if no class is specified.
+
+        Args:
+            classname (str, optional): Name of the class to retrieve instances for.
+
+        Returns:
+            list[str]: Instances as strings.
+        """
+        if classname is None:
+            return [str(obj) for obj in self.__objects.values()]
+        
+        return [str(v) for k, v in self.__objects.items() if k.startswith(classname)]
+
+    def update_instance(self, obj_key, attr_name, attr_val):
+        """_summary_
+
+        Args:
+            obj_key (_type_): _description_
+            attr_name (_type_): _description_
+            attr_val (_type_): _description_
+        """
+        obj = self.get_instance(obj_key)
+        if obj:            
+            attr_type = type(getattr(obj, attr_name, ''))
+            attr_val = attr_type(attr_val)
+            setattr(obj, attr_name, attr_val)
+
+    
